71788c4928a17f5580ada723462b81d813a2b189
amritesh-bhu <amriteshbhu438@gmail.com>
2025-08-04 23:56:40 +0530

    Removed unwanted code...

--- src/routes/user-profile.js
+++ src/routes/user-profile.js
@@ -1,5 +1,4 @@
 import { userDomain } from "../domain/auth/index.js"
-import { handleRoute } from "../lib/handleRoute.js"
 import { userSession } from "../middlewares/userSession.js"
 import { datavalidation } from "../utils/validateData.js"
 import express from "express"
@@ -8,19 +7,29 @@ import express from "express"
 export const profileRouter = express.Router()
 
 profileRouter.get('/view', userSession, async (req, res) => {
-    const user = req.user
-    if (!user) {
-        throw new Error("User does not exist!")
+    try {
+        const user = req.user
+        if (!user) {
+            throw new Error("User does not exist!")
+        }
+        res.send(user)
+    } catch (err) {
+        res.status(404).json({ message: err.message })
     }
-    res.send(user)
+
 })
 
 profileRouter.patch('/updates', userSession, async (req, res) => {
 
-    if (!datavalidation.validateDataToBeUpdated(req)) {
-        throw new Error("Please update the allowed fields!")
-    }
+    try {
+        if (!datavalidation.validateDataToBeUpdated(req)) {
+            throw new Error("Please update the allowed fields!")
+        }
 
-    const user = await userDomain.profileUpdate(req.body, req.user)
-    res.json({ message: "user data updated successfully" })
+        const user = await userDomain.profileUpdate(req.body, req.user)
+        res.json({ message: "user data updated successfully" })
+
+    } catch (err) {
+        res.status(404).json({ message: err.message })
+    }
 })
