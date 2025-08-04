c7812423e81faf586e0ea689a6dfbd092b93d2fe
amritesh-bhu <amriteshbhu438@gmail.com>
2025-08-04 23:54:11 +0530

    code clean up!!

--- src/config/dbCon.js
+++ src/config/dbCon.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
 
-export const dbConn = async (uri) =>{
+export const dbConn = async (uri) => {
     try {
-        await mongoose.connect(uri)        
+        await mongoose.connect(uri)
         console.log("database connected successfully!")
     } catch (error) {
         throw new Error(`Something went wrong with the database connection ${error.message}`)
